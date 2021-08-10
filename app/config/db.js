f6a5ea5a7f84007ed50d4b3149fe8aa4d84aca9d
Dias <fire.kiborgik@gmail.com>
2021-08-10 11:33:07 +0600

    fix 1.2

--- app/config/db.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  db: 'mongodb+srv://weabits:antr0p0l0g@cluster0.q324e.mongodb.net/<dbname>?retryWrites=true&w=majority',
-  //db: 'mongodb://localhost:27017/website',
-  secret: 'some_itproger'
-}
